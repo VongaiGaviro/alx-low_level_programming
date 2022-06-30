f118194f7075e4d7d9a265563878b616593e297c
Vongai Gaviro <vgaviro@gmail.com>
2022-06-30 13:55:26 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h> 
- /** 
-  * main - Entry point 
-  * 
-  * Return: Always 0 (success) 
-  */ 
+/** 
+*main - Entry point 
+* 
+*Return: Always 1 (success) 
+*/ 
   
- int main(void) 
-	 { 
-		 char stri[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-			write(2, stri, 59); 
-		        	return (1); 
-		     }
+int main(void) 
+	{ 
+		char stri[]  =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		write(2,stri,59);
+	       	return (1);
+	 }
