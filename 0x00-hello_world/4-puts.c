9582e223f120cf5bb275f174ea44cfe2916529cc
Vongai Gaviro <vgaviro@gmail.com>
2022-06-30 11:43:52 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,10 @@
 
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 
