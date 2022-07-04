1633a5c4e74380053410d8e9fbeb0acca5173ea6
VongaiGaviro <106873714+VongaiGaviro@users.noreply.github.com>
2022-07-04 06:18:00 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <unistd.h> 
-/** 
-*main - Entry point 
-* 
-*Return: Always 1 (success) 
-*/ 
-  
-int main(void) 
-	{ 
-		char stri[]  =  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-		write(2,stri,59);
-	       	return (1);
-	 }
+echo '#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		return (1);
+	}'>101-quote.c
