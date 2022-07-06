05b177c2bebbdb625ed5bf8fcae4e284aa771029
VongaiGaviro <106873714+VongaiGaviro@users.noreply.github.com>
2022-07-06 03:20:04 +0200

    Update 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet in lower case.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
